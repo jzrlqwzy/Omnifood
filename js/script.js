db80dd2bda03a0e4503ee49a636c56ac9250d1b5
lei <littlesomerest@gmail.com>
2024-04-20 13:49:21 +0800

    smooth scrolling animation in CSS way and in JS way

--- js/script.js
+++ js/script.js
@@ -5,6 +5,32 @@ const headerEl = document.querySelector("header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+///////////////////////////////////////////////////////////
+// Smooth Scrolling Animation in JS way
+const allLinks = document.querySelectorAll("a:link");
+console.log(allLinks);
+allLinks.forEach((link) =>
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  })
+);
+
 ///////////////////////////////////////////////////////////
 // Sticky navigation
 const sectionHeroEl = document.querySelector(".section-hero");
